f199d30dc5d3f45aea23e8eb75d49280d368c5fc
Андрей Гостев <gostev.a@perfluence.io>
2025-09-09 11:41:56 +0300

    Version 4.0-alpha.1 Filesystem/Utils tests

---
+++ tests/DataProvider/Filesystem/UtilsProvider.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AndrewGos\TelegramBot\Tests\DataProvider\Filesystem;
+
+use AndrewGos\TelegramBot\Filesystem\Utils;
+
+class UtilsProvider
+{
+    public static function normalizePathProvider(): array
+    {
+        $ds = DIRECTORY_SEPARATOR;
+        return [
+            'simple' => ['/a/b/c', "{$ds}a{$ds}b{$ds}c"],
+            'mixed_slashes' => ['/a\\b/c', "{$ds}a{$ds}b{$ds}c"],
+            'dots' => ['/a/./b/../c', "{$ds}a{$ds}c"],
+            'double_dots_root' => ['/../a/b', "{$ds}a{$ds}b"],
+            'trailing_slash' => ['/a/b/', "{$ds}a{$ds}b"],
+            'empty' => ['', ''],
+            __DIR__ => [__DIR__, __DIR__],
+            '~' => ['~', Utils::getHomeDir()],
+        ];
+    }
+
+    public static function splitProvider(): array
+    {
+        $ds = DIRECTORY_SEPARATOR;
+        return [
+            'simple' => ['/a/b/c', [$ds, "a{$ds}b{$ds}c"]],
+            'mixed_slashes' => ["/a\\b/c", [$ds, "a\\b{$ds}c"]],
+            'dots' => ['/a/./b/../c', [$ds, "a{$ds}.{$ds}b{$ds}..{$ds}c"]],
+            'empty' => ['', ['', '']],
+            'scheme' => ['https://a/b/c', ['https://', "a{$ds}b{$ds}c"]],
+            'windows_short' => ['C:', ['C:' . DIRECTORY_SEPARATOR, '']],
+            'windows' => ['C:' . DIRECTORY_SEPARATOR . 'a/b/c', ['C:' . DIRECTORY_SEPARATOR, "a{$ds}b{$ds}c"]],
+        ];
+    }
+
+    public static function homeDirProvider(): array
+    {
+        $ds = DIRECTORY_SEPARATOR;
+        return [
+            // Unix
+            'simple' => ['/a/b/c', "{$ds}a{$ds}b{$ds}c"],
+            'mixed_slashes' => ["/a\\b/c", "{$ds}a{$ds}b{$ds}c"],
+
+            // Windows
+            'windows' => [["C:", '/a/b/c'], "C:{$ds}{$ds}a{$ds}b{$ds}c"],
+            'windows_slashes' => [["C:", '\\a\\b\\c'], "C:{$ds}{$ds}a{$ds}b{$ds}c"],
+
+            // Error
+            'error' => ['', ''],
+        ];
+    }
+}
---
+++ tests/Filesystem/UtilsTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AndrewGos\TelegramBot\Tests\Filesystem;
+
+use AndrewGos\TelegramBot\Filesystem\Utils;
+use AndrewGos\TelegramBot\Tests\DataProvider\Filesystem\UtilsProvider;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProviderExternal;
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+
+#[CoversClass(Utils::class)]
+class UtilsTest extends TestCase
+{
+    #[DataProviderExternal(UtilsProvider::class, 'normalizePathProvider')]
+    public function testNormalize(string $input, string $expected): void
+    {
+        $this->assertSame($expected, Utils::normalize($input));
+    }
+    #[DataProviderExternal(UtilsProvider::class, 'splitProvider')]
+    public function testSplit(string $input, array $expected): void
+    {
+        $this->assertSame($expected, Utils::split($input));
+    }
+
+    #[DataProviderExternal(UtilsProvider::class, 'homeDirProvider')]
+    public function testGetHomeDir(string|array $input, string $expected): void
+    {
+        // Emulate Unix
+        if (is_string($input)) {
+            putenv("HOME=$input");
+            if (!$input) {
+                $this->expectException(RuntimeException::class);
+            }
+            $result = Utils::getHomeDir();
+            $this->assertSame($expected, $result);
+        } else {
+            // Emulate Windows
+            putenv("HOMEDRIVE=$input[0]");
+            putenv("HOMEPATH=$input[1]");
+            putenv('HOME=');
+
+            $this->assertSame($expected, Utils::getHomeDir());
+        }
+    }
+}
