b846e4fa121f208ad2cf345c87ff6b227776cf88
Андрей Гостев <gostev.a@perfluence.io>
2025-12-25 00:41:59 +0300

    fix: 4.0.6 fix: fix chat id constructor

--- src/ValueObject/ChatId.php
+++ src/ValueObject/ChatId.php
@@ -8,17 +8,23 @@ use AndrewGos\TelegramBot\Exception\InvalidValueObjectConfigException;
 #[CanBeBuiltFromScalar]
 readonly class ChatId
 {
+    private string|int $id;
+
     /**
      * @param string|int $id
      *
      * @throws InvalidValueObjectConfigException
      */
     public function __construct(
-        private string|int $id,
+        string|int $id,
     ) {
-        if (is_string($this->id) && !preg_match('/^@[A-z\d]{5,32}$/ui', $this->id)) {
+        if (is_string($id) && is_numeric($id)) {
+            $id = intval($id);
+        }
+        if (is_string($id) && !preg_match('/^@[A-z\d]{5,32}$/ui', $id)) {
             throw new InvalidValueObjectConfigException(self::class, 'invalid chat id representation');
         }
+        $this->id = $id;
     }
 
     public function getId(): int|string
